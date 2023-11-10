a995cb3f509ab6590a37f72a62ebfb71b39c9b91
Rhaegal222 <rhaegal222@gmail.com>
2023-11-10 10:55:43 +0100

    Implement quicksort algorithm for sorting integers in a list

--- src/main/java/it/unical/ingsw/MyListUtil.java
+++ src/main/java/it/unical/ingsw/MyListUtil.java
@@ -4,12 +4,36 @@ import java.util.Collections;
 import java.util.List;
 
 public class MyListUtil {
+
     public static List<Integer> sort(List<Integer> list, int order) {
-        if (order == 0) {
-            Collections.sort(list);
-        } else if (order == 1) {
-            Collections.sort(list, Collections.reverseOrder());
+        if (list == null || list.isEmpty()) {
+            return list;
+        }
+        quicksort(list, 0, list.size() - 1);
+        if (order > 0) {
+            Collections.reverse(list);
         }
         return list;
+    }
+
+    private static void quicksort(List<Integer> list, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(list, left, right);
+            quicksort(list, left, pivotIndex - 1);
+            quicksort(list, pivotIndex + 1, right);
+        }
+    }
+
+    private static int partition(List<Integer> list, int left, int right) {
+        int pivot = list.get(right);
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (list.get(j) <= pivot) {
+                i++;
+                Collections.swap(list, i, j);
+            }
+        }
+        Collections.swap(list, i + 1, right);
+        return i + 1;
     }
 }
--- src/test/java/it/unical/ingsw/MyListUtilTest.java
+++ src/test/java/it/unical/ingsw/MyListUtilTest.java
@@ -5,30 +5,56 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MyListUtilTest {
-    private List<Integer> list;
 
-    @Before
-    public void setUp() {
-        list = new ArrayList<>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5));
+    @Test
+    public void testSortEmptyList() {
+        List<Integer> emptyList = new ArrayList<>();
+        List<Integer> sortedList = MyListUtil.sort(emptyList, 1);
+        assertEquals(emptyList, sortedList);
     }
 
     @Test
-    public void testSortAscending() {
-        List<Integer> sortedList = MyListUtil.sort(list, 1);
+    public void testSortNullList() {
+        List<Integer> nullList = null;
+        List<Integer> sortedList = MyListUtil.sort(nullList, 1);
+        assertNull(sortedList);
+    }
+
+    @Test
+    public void testSortAscendingWithDuplicates() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5));
+        List<Integer> sortedList = MyListUtil.sort(list, 0);
         assertEquals(Arrays.asList(1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9), sortedList);
     }
 
     @Test
-    public void testSortDescending() {
-        List<Integer> sortedList = MyListUtil.sort(list, -1);
+    public void testSortDescendingWithDuplicates() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5));
+        List<Integer> sortedList = MyListUtil.sort(list, 1);
         assertEquals(Arrays.asList(9, 6, 5, 5, 5, 4, 3, 3, 2, 1, 1), sortedList);
     }
 
+    @Test
+    public void testSortAscendingWithNegativeNumbers() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(-3, 1, -4, 1, 5, 9, -2, 6, 5, -3, 5));
+        List<Integer> sortedList = MyListUtil.sort(list, 0);
+        assertEquals(Arrays.asList(-4, -3, -3, -2, 1, 1, 5, 5, 5, 6, 9), sortedList);
+    }
+
+    @Test
+    public void testSortDescendingWithNegativeNumbers() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(-3, 1, -4, 1, 5, 9, -2, 6, 5, -3, 5));
+        List<Integer> sortedList = MyListUtil.sort(list, 1);
+        assertEquals(Arrays.asList(9, 6, 5, 5, 5, 1, 1, -2, -3, -3, -4), sortedList);
+    }
+    
     @Before
     public void printStart() {
         DateTime start = new DateTime();
