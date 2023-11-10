ccf3b2f50f216f1a967e60f65479b6677ff8baca
Rhaegal222 <rhaegal222@gmail.com>
2023-11-10 02:54:31 +0100

    Sviluppo terminato

--- src/main/java/it/unical/ingsw/MyListUtil.java
+++ src/main/java/it/unical/ingsw/MyListUtil.java
@@ -1,2 +1,15 @@
-package it.unical.ingsw;public class MyListUtil {
+package it.unical.ingsw;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MyListUtil {
+    public static List<Integer> sort(List<Integer> list, int order) {
+        if (order == 0) {
+            Collections.sort(list);
+        } else if (order == 1) {
+            Collections.sort(list, Collections.reverseOrder());
+        }
+        return list;
+    }
 }
--- src/test/java/it/unical/ingsw/MyListUtilTest.java
+++ src/test/java/it/unical/ingsw/MyListUtilTest.java
@@ -1,2 +1,29 @@
-package it.unical.ingsw;public class MyListUtilTest {
+package it.unical.ingsw;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyListUtilTest {
+    private List<Integer> list;
+
+    @Before
+    public void setUp() {
+        list = new ArrayList<>(Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5));
+    }
+
+    @Test
+    public void testSortAscending() {
+        List<Integer> sortedList = MyListUtil.sort(list, 1);
+        assertEquals(Arrays.asList(1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9), sortedList);
+    }
+
+    @Test
+    public void testSortDescending() {
+        List<Integer> sortedList = MyListUtil.sort(list, -1);
+        assertEquals(Arrays.asList(9, 6, 5, 5, 5, 4, 3, 3, 2, 1, 1), sortedList);
+    }
 }
